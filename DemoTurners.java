0be545b05526346ac322a42c7d9cc0770584bb5f
mohamadrkamaei <mohamadrkamaei@gmail.com>
2019-04-19 20:07:25 +0430

    create page and pancake class and demo turners application

---
+++ DemoTurners.java
@@ -0,0 +1,14 @@
+package hw5;
+
+public class DemoTurners {
+
+    public static void main(String[] args) {
+	// write your code here
+        Leaf leaf =new Leaf();
+        Page page =new Page();
+        Pancake pancake =new Pancake();
+        leaf.turn();
+        page.turn();
+        pancake.turn();
+    }
+}
---
+++ Pancake.java
@@ -0,0 +1,8 @@
+package hw5;
+
+public class Pancake implements Turner {
+    @Override
+    public void turn() {
+        System.out.println(" Flipping");
+    }
+}

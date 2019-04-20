bc265e757b40b51816436e9a37a1b085b6fb3b6f
mohammad <mohammad.hashemi747@gmail.com>
2019-04-20 13:38:12 +0430

    Rename the DemoTurner to DemoTurner2 and creatin objects of FlippingGame and Cooler classes.

--- src/DemoTurners.java
+++
@@ -1,9 +0,0 @@
-public class DemoTurners {
-    public static void main(String[] args) {
-        Leaf leaf = new Leaf();
-        leaf.turn();
-
-        Pancake pancake = new Pancake();
-        pancake.turn();
-    }
-}
---
+++ src/DemoTurners2.java
@@ -0,0 +1,15 @@
+public class DemoTurners2 {
+    public static void main(String[] args) {
+        Leaf leaf = new Leaf();
+        leaf.turn();
+
+        Pancake pancake = new Pancake();
+        pancake.turn();
+
+        FlippingGame flippingGame = new FlippingGame();
+        flippingGame.turn();
+
+        Cooler cooler = new Cooler();
+        cooler.turn();
+    }
+}

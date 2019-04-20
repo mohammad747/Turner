f9c765a18e3593964ec5eb5402860c93e3728f6e
mohammad <mohammad.hashemi747@gmail.com>
2019-04-20 13:28:19 +0430

    Crate DemoTurners and demonstrates the turn() method for each class.

---
+++ src/DemoTurners.java
@@ -0,0 +1,9 @@
+public class DemoTurners {
+    public static void main(String[] args) {
+        Leaf leaf = new Leaf();
+        leaf.turn();
+
+        Pancake pancake = new Pancake();
+        pancake.turn();
+    }
+}
